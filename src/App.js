bb85c4131622a0d11c52abee48cf87208e46e0dd
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-07-19 19:46:15 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1><code>$ rave.directory</code></h1>
+        <a href="https://rave.domains"><button style={{
+            border: 'none',
+            background: '#272727',
+            color: '#FFF',
+            cursor: 'pointer',
+            borderRadius: '15px',
+            padding: '2vh 4vh',
+            fontFamily: 'Nunito Sans',
+            fontSize: '21px'}}>
+            {">"} <code>Website</code>
+          </button>
+        </a>
+        <a href="https://discord.gg/RWnNZff4PJ"><button style={{
+            border: 'none',
+            background: '#272727',
+            color: '#FFF',
+            cursor: 'pointer',
+            borderRadius: '15px',
+            padding: '2vh 4vh',
+            fontFamily: 'Nunito Sans',
+            fontSize: '21px'}}>
+            {">"} <code>Discord</code>
+          </button>
+        </a>
+        <a href="https://twitter.com/rave_names"><button style={{
+            border: 'none',
+            background: '#272727',
+            color: '#FFF',
+            cursor: 'pointer',
+            borderRadius: '15px',
+            padding: '2vh 4vh',
+            fontFamily: 'Nunito Sans',
+            fontSize: '21px'}}>
+            {">"} <code>Twitter</code>
+          </button>
+        </a>
+        <a href="https://docs.rave.domains/"><button style={{
+            border: 'none',
+            background: '#272727',
+            color: '#FFF',
+            cursor: 'pointer',
+            borderRadius: '15px',
+            padding: '2vh 4vh',
+            fontFamily: 'Nunito Sans',
+            fontSize: '21px'}}>
+            {">"} <code>Documentation</code>
+          </button>
+        </a>
+        <a href="https://t.me/OHMKI"><button style={{
+            border: 'none',
+            background: '#272727',
+            color: '#FFF',
+            cursor: 'pointer',
+            borderRadius: '15px',
+            padding: '2vh 4vh',
+            fontFamily: 'Nunito Sans',
+            fontSize: '21px'}}>
+            {">"} <code>Business Enquiries</code>
+          </button>
         </a>
       </header>
     </div>
